bd662a0c081dae46b41b945625c38d9751306758
Kathryn Rhoads <46656679+rhokath@users.noreply.github.com>
2019-06-13 16:04:44 -0400

    working on lambda-classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,61 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(attributes){
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.location;
+    }
+    speak(){
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`)
+
+    }//speak method
+}//Person class
+
+class Instructor extends Person {
+    constructor(attributes){
+        super(attributes);
+        this.speciality = attributes.speciality;
+        this.favLanguage = attributes.favLanguage;
+        this.catchPhrase = attributes.catchPhrase;      
+    }
+    demo(subject){
+        console.log(`Today we are learning about ${subject}`)
+    }
+    grade(student, subject) {
+        `${student.name} receives a perfect score on ${subject}`
+    }
+}
+
+class Student extends Person {
+    constructor(attributes){
+        super(attributes);
+        this.previousBackground = attributes.previousBackground;
+        this.className = attributes.className;
+        this.favSubjects = attributes.favSubjects;    
+    }
+    listsSubjects(){
+
+    }//listsSubjects method
+    PRAssignment(){
+       console.log(`${student.name} has submitted a PR for ${subject}`) 
+
+    }//PRAssignment method
+    sprintChallenge(){
+        console.log(`${student.name} has begun sprint challenge on ${subject}`)
+    }//sprintChallenge method
+}//Student class
+
+class PM extends Instructor {
+    constructor(attributes){
+        super(attributes);
+        this.gradClassName = attributes.gradClassName;
+        this.favInstructor = attributes.favInstructor;
+    }
+    standUp(){
+
+    }//standUp method
+    debugsCode(){
+
+    }//debugsCode method
+}//PM class
