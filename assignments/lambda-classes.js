55b7323d81c077e01f56181b7ad89905af2dee67
Kathryn Rhoads <46656679+rhokath@users.noreply.github.com>
2019-06-13 17:14:46 -0400

    made my objects

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -7,55 +7,119 @@ class Person {
         this.location = attributes.location;
     }
     speak(){
-        console.log(`Hello my name is ${this.name}, I am from ${this.location}`)
+        `Hello my name is ${this.name}, I am from ${this.location}`
 
     }//speak method
 }//Person class
 
 class Instructor extends Person {
-    constructor(attributes){
-        super(attributes);
-        this.speciality = attributes.speciality;
-        this.favLanguage = attributes.favLanguage;
-        this.catchPhrase = attributes.catchPhrase;      
+    constructor(instAttributes){
+        super(instAttributes);
+        this.speciality = instAttributes.speciality;
+        this.favLanguage = instAttributes.favLanguage;
+        this.catchPhrase = instAttributes.catchPhrase;      
     }
     demo(subject){
-        console.log(`Today we are learning about ${subject}`)
+        return `Today we are learning about ${subject}`
     }
     grade(student, subject) {
-        `${student.name} receives a perfect score on ${subject}`
+        return `${student.name} receives a perfect score on ${subject}`
     }
 }
 
 class Student extends Person {
-    constructor(attributes){
-        super(attributes);
-        this.previousBackground = attributes.previousBackground;
-        this.className = attributes.className;
-        this.favSubjects = attributes.favSubjects;    
+    constructor(stuAttributes){
+        super(stuAttributes);
+        this.previousBackground = stuAttributes.previousBackground;
+        this.className = stuAttributes.className;
+        this.favSubjects = stuAttributes.favSubjects;    
     }
     listsSubjects(){
 
     }//listsSubjects method
-    PRAssignment(){
-       console.log(`${student.name} has submitted a PR for ${subject}`) 
+    PRAssignment(subject){
+       return `${student.name} has submitted a PR for ${subject}`
 
     }//PRAssignment method
-    sprintChallenge(){
-        console.log(`${student.name} has begun sprint challenge on ${subject}`)
+    sprintChallenge(subject){
+        return `${student.name} has begun sprint challenge on ${subject}`
     }//sprintChallenge method
 }//Student class
 
 class PM extends Instructor {
-    constructor(attributes){
-        super(attributes);
-        this.gradClassName = attributes.gradClassName;
-        this.favInstructor = attributes.favInstructor;
+    constructor(pmAttributes){
+        super(pmAttributes);
+        this.gradClassName = pmAttributes.gradClassName;
+        this.favInstructor = pmAttributes.favInstructor;
     }
-    standUp(){
-
+    standUp(channel){
+        return `${this.name} announces to ${channel}, @channel standy times!​​​​​`
     }//standUp method
-    debugsCode(){
-
+    debugsCode(student, subject){
+        return `${this.name} debugs ${student.name}'s code on ${subject}`
     }//debugsCode method
 }//PM class
+
+//Instructors//
+
+const dan = new Instructor ({
+    name: "Dan",
+    age: 39,
+    location: "California",
+    favLanguage: "C#",
+    speciality: "Front-end",
+    catchPhrase: "Google it!",
+})
+
+const shelly = new Instructor({
+    name: "Shelly",
+    age: 38,
+    location: "Texas",
+    favLanguage: "JS",
+    speciality: "Back-end",
+    catchPhrase: "Figure it out",
+})
+
+//Students//
+
+const joe = new Student ({
+    name: "Joe",
+    age: 25,
+    location: "Canada",
+    previousBackground: "dancer",
+    className: "Web15",
+    favSubjects: ["math", "python", "physics"]
+})
+
+const diane = new Student ({
+    name: "Diane",
+    age: 28,
+    location: "Georgia",
+    previousBackground: "painter",
+    className: "Web14",
+    favSubjects: ["javascript", "CSS", "HTML"]
+})
+
+//PMs//
+
+const fred = new PM ({
+    name: "Fred",
+    age: 30,
+    location: "Maine",
+    speciality: "CSS",
+    favLanguage: "elm",
+    catchPhrase: "love it",
+    gradClassName: "Web19",
+    favInstructor: "Josh",
+})
+
+const lily = new PM ({
+    name: "Lily",
+    age: 33,
+    location: "Detroit",
+    speciality: "react",
+    favLanguage: "Ruby",
+    catchPhrase: "awesome job",
+    gradClassName: "CS18",
+    favInstructor: "Josh",
+})
